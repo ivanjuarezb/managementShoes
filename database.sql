b1d15692133b26fbe19e61f81eb4ff28efcc1d39
ivanjuarezb <ivanjuarezb@outlook.es>
2021-08-09 20:11:39 +0200

    Completed database

--- database.sql
+++ database.sql
@@ -2,15 +2,16 @@ CREATE DATABASE NOT EXISTS dbshoe CHARACTER SET 'utf8' COLLATE 'utf8_general_ci 
 USE dbshoe;
 
 CREATE  TABLE shoes(
-    id           INT(255) auto_increment NOT NULL,
-    name         VARCHAR(255) NOT NULL,
+    id           INT(50 auto_increment NOT NULL,
+    name         VARCHAR(50) NOT NULL,
+    gender       VARCHAR(50) NOT NULL,
     style        VARCHAR(50) NOT NULL,
     color        VARCHAR(50) NOT NULL,
-    size         INT(100) NOT NULL,
-    units        INT(100) NOT NULL,
+    size         INT(50) NOT NULL,
+    units        INT(50) NOT NULL,
     price        DECIMAL(4,2) NOT NULL,
     datetime     DATETIME NOT NULL,
-    nameimage    VARCHAR(255) NOT NULL,
+    nameimage    VARCHAR(50) NOT NULL,
     image        LONGBLOB NOT NULL,
     type         VARCHAR(50) NOT NULL,
 
